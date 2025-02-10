99f3905c7dccc9e3a59e42dfdd7f60ecc38c47fc
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-02-11 01:14:40 +0600

    button updated

--- src/components/ContactUsPage/ContactUs.jsx
+++ src/components/ContactUsPage/ContactUs.jsx
@@ -183,7 +183,7 @@ const CustomerCare = () => {
           </div>
           <button
             type="submit"
-            className="bg-yellow-500 text-black py-2 px-4 rounded hover:bg-yellow-600"
+            className="bg-yellow-500 text-white font-bold py-2 px-4 rounded hover:bg-yellow-600"
           >
             Submit
           </button>
